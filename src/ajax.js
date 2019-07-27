d5d3ac7b1a2bf5620e1da0e917c5dc7b252aeb0c
coderyyx <coderyyx@gmail.com>
2019-07-27 12:08:04 +0800

    add eslint

--- src/ajax.js
+++ src/ajax.js
@@ -1,77 +1,70 @@
 /**
  * api fetch
  */
-import $ from 'jquery';
+import $ from 'jquery'
 
-const noop = ()=>{};
+const noop = () => {}
 const shouldWithCookie = (withCredentials) => {
-    if (withCredentials) {
-        return {
-            xhrFields: {
-                withCredentials: true
-              }
-        }
+  if (withCredentials) {
+    return {
+      xhrFields: {
+        withCredentials: true
+      }
     }
-    return {};
+  }
+  return {}
 }
-const request = (requestObj) =>{
-    let {
-            method = 'POST',
-            data = {},
-            url,
-            async = true,
-            contentType = "application/x-www-form-urlencoded;charset=utf-8;",
-            timeout = 20*1000,
-            request_start,
-            request_receive_data,
-            request_error,
-            successAlert = window.alert,
-            errorAlert = window.alert,
-            sessionTimeOut = noop,
-            equalsField = 'result',
-            errorField = 'message',
-            withCredentials,
-            beforeSend = noop,
-        } = requestObj;
-    
+const request = (requestObj) => {
+  const {
+    method = 'POST',
+    data = {},
+    url,
+    async = true,
+    contentType = 'application/x-www-form-urlencoded;charset=utf-8;',
+    timeout = 20 * 1000,
+    requestStart,
+    requestReceiveData,
+    requestError,
+    errorAlert = window.alert,
+    sessionTimeOut = noop,
+    equalsField = 'result',
+    errorField = 'message',
+    withCredentials,
+    beforeSend = noop
+  } = requestObj
 
-    if(!url){
-        errorAlert('url can not be empty!');
-        return;
+  if (!url) {
+    errorAlert('url can not be empty!')
+    return
+  }
+
+  // request start
+  requestStart()
+  const cookieConfig = shouldWithCookie(withCredentials)
+  // web request
+  $.ajax({
+    type: method,
+    url,
+    data,
+    contentType,
+    async,
+    timeout,
+    ...cookieConfig,
+    beforeSend: function (xhr) {
+      beforeSend(xhr)
+    },
+    success: function (data) {
+      if (typeof data === 'string') { data = JSON.parse(data) }
+      if (data[equalsField] && data[equalsField] === 'success') { requestReceiveData(data) } else {
+        errorAlert(data[errorField] ? data[errorField] : 'fail')
+        requestError(data)
+      }
+    },
+    error: function (xhr, status, err) {
+      sessionTimeOut(xhr)
+      requestError(err)
     }
+  })
+}
 
-    //request start
-    request_start();
-    const cookieConfig = shouldWithCookie(withCredentials);
-    //web request
-    $.ajax({
-        type : method,
-        url ,
-        data ,
-        contentType ,
-        async ,
-        timeout ,
-        ...cookieConfig,
-        beforeSend: function(xhr) { 
-            beforeSend(xhr);
-        },
-        success : function(data){
-            if(typeof data === 'string')
-                data = JSON.parse(data);
-            if(data[equalsField] && data[equalsField]==='success')
-                request_receive_data(data);
-            else{
-                errorAlert(data[errorField] ? data[errorField] : 'fail');
-                request_error(data);
-            }
-                
-        },
-        error : function(xhr, status, err){
-            sessionTimeOut(xhr);
-            request_error(err);
-            console.log(err);
-        }
-    })
- }
-
- export default request;
+export default request
--- src/index.js
+++ src/index.js
@@ -1,65 +1,60 @@
-import { isArray } from "util";
-import $ from 'jquery';
-import request from './ajax';
+import request from './ajax'
 
-const simplifyMiddleware = options => store => next => action =>{
-
-    let {dispatch,getState} = store;
-
-    // init configObject
-    let { 
-            url,
-            types = [null,null,null]
-        } = action;
-    /**
+const simplifyMiddleware = options => store => next => action => {
+  // init configObject
+  const {
+    url,
+    types = [null, null, null]
+  } = action
+  /**
      * 以url为非标准action唯一依据
      * 标准action进入下一个中间件
      */
-    if(!url){
-        next(action);
-        //退出当前中间件
-        return;
-    }
+  if (!url) {
+    next(action)
+    // 退出当前中间件
+    return
+  }
 
-    /**
+  /**
      * 自定义action
      */
-    const [LOADING,SUCCESS,ERROR] = types;
-    
-    //request start
-    function request_start(next,LOADING){
-        return ()=>next({
-            type : LOADING,
-            path : 'LOADING'
-        });
-    }
+  const [LOADING, SUCCESS, ERROR] = types
 
-    //receive data
-    function request_receive_data(next,SUCCESS){
-        return (result)=>next({
-            type : SUCCESS,
-            path : 'LOADED',
-            data : result
-        })
-    }
-
-    //network error or server error
-    function request_error(next,ERROR){
-        return (result)=>next({
-            type : ERROR,
-            path : 'LOADED',
-            data : result
-        });
-    }
-
-    //异步请求
-    request({
-        request_start:request_start(next,LOADING),
-        request_receive_data:request_receive_data(next,SUCCESS),
-        request_error:request_error(next,ERROR),
-        ...options,
-        ...action
+  // request start
+  function requestStart (next, LOADING) {
+    return () => next({
+      type: LOADING,
+      path: 'LOADING'
     })
+  }
+
+  // receive data
+  function requestReceiveData (next, SUCCESS) {
+    return (result) => next({
+      type: SUCCESS,
+      path: 'LOADED',
+      data: result
+    })
+  }
+
+  // network error or server error
+  function requestError (next, ERROR) {
+    return (result) => next({
+      type: ERROR,
+      path: 'LOADED',
+      data: result
+    })
+  }
+
+  // 异步请求
+  request({
+    requestStart: requestStart(next, LOADING),
+    requestReceiveData: requestReceiveData(next, SUCCESS),
+    requestError: requestError(next, ERROR),
+    ...options,
+    ...action
+  })
 }
 
-export default simplifyMiddleware;
+export default simplifyMiddleware
